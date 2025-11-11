2b07849a2199470702738bcf100909f774752e2e
Priyanka <priyanka@antheminfotech.com>
2025-11-11 16:46:08 +0530

    footer fixed

--- components/FooterComponent/FooterLinkItem.jsx
+++ components/FooterComponent/FooterLinkItem.jsx
@@ -7,21 +7,37 @@ const FooterLinkItem = ({ data }) => {
   if (!data) return null; // safety check
 
   return (
-    <div className="footer-widget">
-      <h4 className="footer-widget-title">{data.title}</h4>
-      <nav aria-label={data.title}>
-        <ul>
-          {data.list.map((single) => (
-            <li key={single.id}>
-              <Link href={single.url}>
-                {single.text}{" "}
-                {single?.badge && <span className="ft-badge">{single.badge}</span>}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    // <div className="footer-widget">
+    //   <h4 className="footer-widget-title">{data.title}</h4>
+    //   <nav aria-label={data.title}>
+    //     <ul >
+    //       {data.list.map((single) => (
+    //         <li key={single.id}>
+    //           <Link href={single.url}>
+    //             {single.text}{" "}
+    //             {single?.badge && <span className="ft-badge">{single.badge}</span>}
+    //           </Link>
+    //         </li>
+    //       ))}
+    //     </ul>
+    //   </nav>
+    // </div>
+
+
+
+     <div className="footer-widget">
+            <h4 className="footer-widget-title">{data.title}</h4>
+            <div className="footer-widget-content">
+                <ul>
+                    {data.list.map((single,key) =>(
+                        <li key={key}>
+                            <Link href={single.url}>{single.text} {single?.badge && <span className="ft-badge">{single.badge}</span>} </Link>
+                        </li>
+                    ))}
+                    
+                </ul>
+            </div>
+        </div>
   );
 };
 
