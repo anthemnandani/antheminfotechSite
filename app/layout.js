404d4b1805ceb47d73c22ed62afff0330a976d94
Priyanka <priyanka@antheminfotech.com>
2026-01-16 12:14:41 +0530

    Metricool tracking script addedd

--- app/layout.js
+++ app/layout.js
@@ -126,6 +126,23 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
         <ScrollToTop />
         <Footer />
+         {/*  Metricool tracking script (loaded after interactive) */}
+        <Script id="metricool-tracker" strategy="afterInteractive">
+          {`
+            function loadScript(a){
+              var b=document.getElementsByTagName("head")[0],
+                  c=document.createElement("script");
+              c.type="text/javascript;
+              c.src="https://tracker.metricool.com/resources/be.js";
+              c.onreadystatechange=a;
+              c.onload=a;
+              b.appendChild(c);
+            }
+            loadScript(function(){
+              beTracker.t({hash:"ebd005992c92a3083d8882fcd675fdb6"});
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
